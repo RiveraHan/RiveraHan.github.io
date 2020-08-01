8febe3b36714ae939b4f259c9fa894f26c1f3522
Hanzell Rivera <hanzellrivera95@gmail.com>
2020-07-31 21:31:35 -0600

    Agregue middleware

---
+++ js/helpers/anio.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', () => {
+    let anio = new Date().getFullYear();
+    document.getElementById('anio').innerHTML = `&copy${ anio } Hanzell Rivera | Desarrollador Web Nicaragua. Todos los derechos reservados`;
+
+})
