755cd58df6ea2e285dd1856e610845133be4d475
Hanzell Rivera <hanzellrivera95@gmail.com>
2020-09-23 18:19:05 -0600

    metadatas y nueva arquitectura

--- js/middlewares/anio.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('load', () => {
-    let anio = new Date().getFullYear();
-    document.getElementById('anio').innerHTML = `&copy${ anio } Hanzell Rivera | Desarrollador Web Nicaragua. Todos los derechos reservados`;
-
-})
